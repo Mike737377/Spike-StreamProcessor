7efd371c89cffa4c7637682b822a4d2543d8619f
Michael Lyons <Mike737377@users.noreply.github.com>
2016-04-12 19:46:21 +1000

    Initial commit

---
+++ Main.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StreamProcessor
+{
+    public static class Program
+    {
+        private static readonly Random _rand = new Random();
+
+        public static void Main(string[] args)
+        {
+            var stream = new PersonStream();
+
+            stream.AddIndexer(new JohnIndexer());
+            stream.AddAnalyzer(new JohnAnalyzer());
+            //stream.AddAnalyzer(new AgeAnalyzer());
+
+            Console.WriteLine("Inserting ppl");
+            Enumerable.Range(0, 300000).Each(x => stream.PutRecord(CreatePerson()));
+            stream.PutRecord(new Person { Name = "John", Age = 35, Expected = false });
+            Enumerable.Range(0, 300000).Each(x => stream.PutRecord(CreatePerson()));
+            stream.PutRecord(new Person { Name = "John", Age = 2, Expected = true });
+            Enumerable.Range(0, 300000).Each(x => stream.PutRecord(CreatePerson()));
+            stream.PutRecord(new Person { Name = "John", Age = 31, Expected = false });
+
+            Console.WriteLine("Querying records");
+            var stopwatch = Stopwatch.StartNew();
+            var results = stream.Query(new { Name = "john" }, x => x.Age == 2);
+            stopwatch.Stop();
+            Console.WriteLine("Time: {0}s", stopwatch.Elapsed.TotalMilliseconds / 1000);
+
+            foreach (var a in results)
+            {
+                Console.WriteLine(a);
+            }
+        }
+
+        private static Person CreatePerson()
+        {
+            return new Person { Name = Guid.NewGuid().ToString().Substring(0, 6), Age = _rand.Next(1, 100), Expected = false };
+        }
+
+    }
+
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public bool Expected { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("{0}, {1}... {2}", Name, Age, Expected);
+        }
+    }
+
+    public class PersonStream : Stream<Person>
+    {
+    }
+
+    public class JohnIndexer : StreamIndexer<Person>
+    {
+        public JohnIndexer()
+            : base(x => new { Name = x.Name.ToLowerInvariant() })
+        { }
+    }
+
+    public class JohnAnalyzer : IStreamAnalyzer<Person>
+    {
+        public void Analyze(Person item)
+        {
+            if (item.Name == "John")
+            {
+                Console.WriteLine("Found john!!!!!!");
+            }
+        }
+    }
+
+    public class AgeAnalyzer : IStreamAnalyzer<Person>
+    {
+        private int _totalPeople = 0;
+        private decimal _totalAge = 0;
+
+        public void Analyze(Person item)
+        {
+            _totalPeople++;
+            _totalAge += item.Age;
+            var averageAge = _totalAge / _totalPeople;
+
+            Console.WriteLine("{0}... Avg: {1}", _totalPeople, averageAge);
+        }
+    }
+
+
+}
---
+++ Stream.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StreamProcessor
+{
+
+    public class Stream<T>
+    {
+        private readonly ConcurrentDictionary<object, ConcurrentBag<T>> _index = new ConcurrentDictionary<object, ConcurrentBag<T>>();
+        private readonly ConcurrentBag<StreamIndexer<T>> _indexers = new ConcurrentBag<StreamIndexer<T>>();
+        private readonly ConcurrentBag<IStreamAnalyzer<T>> _analyzers = new ConcurrentBag<IStreamAnalyzer<T>>();
+
+        public void AddIndexer(StreamIndexer<T> indexer)
+        {
+            _indexers.Add(indexer);
+        }
+
+        public void AddAnalyzer(IStreamAnalyzer<T> analyzer)
+        {
+            _analyzers.Add(analyzer);
+        }
+
+        public IEnumerable<T> Query(object index, Func<T, bool> query)
+        {
+            ConcurrentBag<T> bag;
+            if (_index.TryGetValue(index, out bag))
+            {                
+                var items = bag.ToList().AsEnumerable();
+
+                foreach (var item in items)
+                {
+                    if (query(item))
+                    {
+                        yield return item;
+                    }
+                }
+            }
+        }
+
+        public void PutRecord(T item)
+        {
+            _indexers.AsParallel().ForAll(x =>
+            {
+                var index = x.CreateIndex(item);
+                _index.TryAdd(index.Index, new ConcurrentBag<T>());
+                _index[index.Index].Add(item);
+            });
+
+            _analyzers.AsParallel().ForAll(x =>
+            {
+                x.Analyze(item);
+            });
+        }
+
+        public void PutRecords(IEnumerable<T> streamOfItems)
+        {
+            foreach (var item in streamOfItems)
+            {
+                PutRecord(item);
+            }
+        }
+    }
+
+    public sealed class StreamIndex<T>
+    {
+        public StreamIndex(object index, T value)
+        {
+            Index = index;
+            Value = value;
+        }
+
+        public object Index { get; private set; }
+        public T Value { get; private set; }
+    }
+
+
+    public abstract class StreamIndexer<T>
+    {
+        private readonly Func<T, object> _indexAlgorithm;
+
+        public StreamIndexer(Func<T, object> indexAlgorithm)
+        {
+            _indexAlgorithm = indexAlgorithm;
+        }
+
+        public StreamIndex<T> CreateIndex(T streamItem)
+        {
+            return new StreamIndex<T>(_indexAlgorithm(streamItem), streamItem);
+        }
+    }
+
+    public interface IStreamAnalyzer<T>
+    {
+        void Analyze(T item);
+    }
+}
